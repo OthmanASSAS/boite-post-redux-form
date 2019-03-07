48b2f56690cf1f40437c5b040382b023db1a076a
Othman ASSAS <o.assas@gop.link>
2019-03-07 12:37:28 +0100

    squelette

---
+++ src/containers/Post.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PostContent from '../components/PostContent'
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                <PostContent/>
+            </div>
+        );
+    }
+}
+
+export default Post;
